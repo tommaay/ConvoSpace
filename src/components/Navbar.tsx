18f4320ad813481aa5686b3897764751054c04e1
tommaay <tommy@tomster.me>
2023-06-25 16:15:14 -0700

    Add signout and user nav dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
+import { UserAccountNav } from "./UserAccountNav";
 
 const Navbar = async () => {
   const session = await getAuthSession();
@@ -9,7 +10,7 @@ const Navbar = async () => {
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl">
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-1">
           <Icons.logo className="w-12 h-12 sm:h-10 sm:w-10" />
           <p className="hidden text-sm font-medium text-zinc-700 md:block">
             ConvoSpace
@@ -17,7 +18,7 @@ const Navbar = async () => {
         </Link>
 
         {session?.user ? (
-          <p>Logged in</p>
+          <UserAccountNav user={session.user} />
         ) : (
           <Link href="sign-in" className={buttonVariants()}>
             Sign In
