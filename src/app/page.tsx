27d424959578dd6c4388f27ee6a394885a285fb8
tommaay <tommy@tomster.me>
2023-07-15 13:32:32 +0700

    Add posts to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-import Link from "next/link";
+import CustomFeed from "@/components/homepage/CustomFeed";
+import GeneralFeed from "@/components/homepage/GeneralFeed";
 import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { Home as HomeIcon } from "lucide-react";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 export default async function Home() {
+  const session = await getAuthSession();
+
   return (
     <>
       <h1 className="text-3xl font-bold md:text-4xl">Your feed</h1>
       <div className="grid grid-cols-1 py-6 md:grid-cols-3 gap-y-4 md:gap-x-4">
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
+
         <div className="order-first overflow-hidden border border-gray-200 rounded-lg h-fit md:order-last">
           <div className="px-6 py-4 bg-emerald-100">
             <p className="font-semibold py-3 flex items-center gap-1.5">
@@ -21,8 +28,8 @@ export default async function Home() {
           <dl className="px-6 py-4 -my-3 text-sm leading-6 divide-y divide-gray-100">
             <div className="flex justify-between py-3 gap-x-4">
               <p className="text-zinc-500">
-                Your personal ConvoSpace threads. Come here to check in with
-                your favorite communities.
+                Your personal Breadit frontpage. Come here to check in with your
+                favorite communities.
               </p>
             </div>
 
